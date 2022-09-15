a05dd9448c1cfd0ea863a1f5d02c958bcd4472b3
OmidRashidiFam <omidRashidiFam@gmail.com>
2022-09-15 07:08:50 +0430

    feat(component): create HomePage component
    create HomePage component, import the style file and export it, then import it to App.js file

---
+++ src/pages/home-page/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home-page.scss";
+
+function HomePage() {
+  return (
+    <div className="container home-page">
+      HomePage
+    </div>
+  );
+}
+
+export default HomePage;
