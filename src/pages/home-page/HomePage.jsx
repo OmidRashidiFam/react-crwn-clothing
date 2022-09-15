0fca96e85da59b19505e61cd282b28693a3a7c6b
OmidRashidiFam <omidRashidiFam@gmail.com>
2022-09-15 07:32:10 +0430

    feat(component): create CategoryContainer component
    create CategoryContainer component, import the style file and export it, then import it to HomePage component

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./home-page.scss";
+import CategoryContainer from "./components/category-container/CategoryContainer";
 
 function HomePage() {
   return (
     <div className="container home-page">
-      HomePage
+      <CategoryContainer />
     </div>
   );
 }
---
+++ src/pages/home-page/components/category-container/CategoryContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./category-container.scss";
+
+class CategoryContainer extends React.Component {
+  // state of component
+  state = {};
+
+  // render component
+  render() {
+    return <div className="category-container">ItemContainer</div>;
+  }
+}
+
+export default CategoryContainer;
